2b0a0f1b197990f4881cc8a0394e2a3ddaabc4ab
Khaq0312 <89060843+Khaq0312@users.noreply.github.com>
2024-03-16 13:50:15 +0700

    Add files via upload
    update

--- 21127623.cpp
+++ 21127623.cpp
@@ -24,6 +24,10 @@ void extractURL(std::string URL, std::string &hostname,std::string&path){
     }
 
 }
+
+//in google.com: "Transfer-Encoding: chunked"
+//in bing.com:   "Transfer-Encoding:  chunked"
+
 std::string Get_Body(std::string response) {
     std::string body;
 
@@ -33,31 +37,34 @@ std::string Get_Body(std::string response) {
         bodyStart += 4; 
 
         // check if has chunked
-        size_t chunkedPos = response.find("Transfer-Encoding: chunked");
+        size_t chunkedPos = response.find("Transfer-Encoding:");
         if (chunkedPos != std::string::npos) {
-            size_t chunkStart = bodyStart;//go to Body
-            while (true) {
-                // Find chunk size
-                size_t nextline = response.find("\r\n", chunkStart);
-                if (nextline == std::string::npos)
-                    break; // End of file
-                
-                std::string chunkSizeStr = response.substr(chunkStart, nextline - chunkStart);
+            chunkedPos = response.find("chunked", chunkedPos + 19);//skip transfer encoding
+            if (chunkedPos != std::string::npos) {
+                size_t chunkStart = bodyStart;//go to Body
+                while (true) {
+                    // Find chunk size
+                    size_t nextline = response.find("\r\n", chunkStart);
+                    if (nextline == std::string::npos)
+                        break; // End of file
+                    
+                    std::string chunkSizeStr = response.substr(chunkStart, nextline - chunkStart);
 
-                size_t chunkSize;
-                std::istringstream(chunkSizeStr) >> std::hex >> chunkSize;//convert hex to dec
+                    size_t chunkSize;
+                    std::istringstream(chunkSizeStr) >> std::hex >> chunkSize;//convert hex to dec
 
-                if (chunkSize == 0)
-                    break;
+                    if (chunkSize == 0)
+                    {
+                        break;
+                    }
 
-                // Go to content chunkedsize\r\nContent
-                chunkStart = nextline + 2;
-                body += response.substr(chunkStart, chunkSize);
-                //end of content\r\nchunksize
-                chunkStart += chunkSize + 2; 
+                    // Go to content chunkedsize\r\nContent
+                    chunkStart = nextline + 2;
+                    body += response.substr(chunkStart, chunkSize);
+                    //end of content\r\nchunksize
+                    chunkStart += chunkSize + 2; 
+                }
             }
-        } else {
-            body = response.substr(bodyStart);
         }
     }
     return body;
@@ -73,11 +80,12 @@ int main(int argc, char* argv[]) {
     std::string URL = argv[1];
     std::string path,host;
     extractURL(URL,host,path);
+    
 
     int port = 80;
 
     //GET request
-    std::string request = "GET " + path +  " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
+    std::string request = "GET " + path +  " HTTP/1.1\r\nHost: " + host + "\r\nConnection: keep-alive" +"\r\n\r\n";
 
     //socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -92,6 +100,7 @@ int main(int argc, char* argv[]) {
         close(sockfd);
         return 1;
     }
+
 
     struct sockaddr_in serverAddr;
     bzero((char*)&serverAddr, sizeof(serverAddr));
@@ -112,11 +121,17 @@ int main(int argc, char* argv[]) {
         close(sockfd);
         return 1;
     }
-
+ 
     // receive
     char buffer[MAX_BUFFER_SIZE];
     std::string response = "";
     int receive;
+    bool detect_CL = 0;//detect first content length in header, in case of unexpected content length in body
+    std::string body = "";
+    long long remaining_len = 0;
+    bool isChunkedCase = 0;
+    std::string temp = "";
+
     while (true) {
         receive = recv(sockfd, buffer, sizeof(buffer), 0);
         if (receive < 0) {
@@ -126,10 +141,55 @@ int main(int argc, char* argv[]) {
             break;
         }
         response.append(buffer, receive);
+        //handle chunked
+        size_t end_chunked = response.find("\r\n0\r\n\r\n");
+        if (end_chunked != std::string::npos) {
+            isChunkedCase = 1;
+            break;
+        }
+
+        //handle content length
+        if(detect_CL == 1 && remaining_len != 0){
+            temp = std::string(buffer, receive);
+            body += temp;
+            remaining_len -= receive;
+        }
+        if(detect_CL == 1 && remaining_len == 0){
+            break;
+        }
+
+        if(detect_CL == 0){
+            size_t content_length = response.find("Content-Length:");
+            if(content_length != std::string::npos)
+            {
+                detect_CL = 1;
+                size_t content_size_pos = content_length + 16;
+                
+                while(response[content_size_pos] != '\r')
+                {
+                    temp += response[content_size_pos];
+                    content_size_pos++;
+                }
+                size_t bodyStart = response.find("\r\n\r\n");
+                bodyStart += 4;
+
+                body = response.substr(bodyStart);
+                remaining_len = stoll(temp);
+                if(body.length() == remaining_len)
+                {
+                    break;
+                }
+                remaining_len-=body.length();
+            }
+        }
     }
     
-    //remove header and chunk
-    std::string body = Get_Body(response);
+    //case: chunked
+    if(isChunkedCase)
+    {
+        body = Get_Body(response);
+    }
+    close(sockfd);
 
     if(body.empty() == 0)
     {
@@ -151,7 +211,5 @@ int main(int argc, char* argv[]) {
     {
         std::cerr<<"Error: No body found"<<std::endl;
     }
-
-    close(sockfd);
     return 0;
 }
